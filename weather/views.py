7681905227a6738f6335607670f3d7fee6ad4194
MShbana <M.Shabana@protonmail.ch>
2019-10-04 07:31:29 +0200

    Get a test city's weather info in a seperate page

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,22 @@
+import os
+import requests
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'weather/home.html')
+    appid = os.environ.get('API_KEY')
+    city = 'Cairo' # Test City
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}'
+    r = requests.get(url).json()
+
+    city_weather = {
+        'city': city,
+        'temp': r['main']['temp'],
+        'desc': r['weather'][0]['description']
+    }
+
+    args = {
+        'city_weather': city_weather
+    }
+
+    return render(request, 'weather/city.html', args)
