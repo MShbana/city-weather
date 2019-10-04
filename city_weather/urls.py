f0ef572ff051ea64c50af5c36269994ce289626a
MShbana <M.Shabana@protonmail.ch>
2019-10-04 09:03:35 +0200

    Implement basic login, logout and registeration functionalities

--- city_weather/urls.py
+++ city_weather/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 
 
+from accounts import views as accounts_views
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 
@@ -23,4 +25,22 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls')),
     path('api/', include('api.urls')),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='accounts/login.html',
+            redirect_authenticated_user=True
+        ),
+        name='login'
+    ),
+    path(
+        'register/',
+        accounts_views.register,
+        name='register'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
